4dfac9b885453069d0cdf30f75a5154e98ded820
gautam-11 <gautambaruah24@gmail.com>
2019-01-26 16:03:35 +0530

    Added backend routes and validation for users logic

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://gautam:23571113mlab@ds055802.mlab.com:55802/devconnect"
+  mongoURI: "mongodb://gautam:23571113mlab@ds055802.mlab.com:55802/devconnect",
+  secretOrKey: "235711131719"
 };
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //DB config
 const db = require("./config/keys").mongoURI;
 
@@ -20,9 +25,11 @@ mongoose
   })
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
 
 // Use Routes
 app.use("/api/users", users);
