2a7188dbdce0d3d605b0ffc75872a98e4765ce6e
gautam-11 <gautambaruah24@gmail.com>
2019-01-27 04:30:02 +0530

    Added Profile model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,109 +1,119 @@
-const express = require("express");
-const router = express.Router();
-const gravatar = require("gravatar");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const secret = require("../../config/keys").secretOrKey;
-const User = require("../../models/User");
-const passport = require("passport");
+const express = require('express')
+const router = express.Router()
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const secret = require('../../config/keys').secretOrKey
+const User = require('../../models/User')
+const passport = require('passport')
 
 // Load input validation
-const validateRegisterInput = require("../../validation/register");
-
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
 // @route GET api/users/test
 // @desc Tests users route
 // @access public
-router.get("/test", (req, res) => {
-  res.json({ msg: "User works!" });
-});
+router.get('/test', (req, res) => {
+  res.json({ msg: 'User works!' })
+})
 
 // @route POST api/users/test
 // @desc Registers users
 // @access public
-router.post("/register", (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
+router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body)
 
-  //Check Validation
+  // Check Validation
   if (!isValid) {
-    return res.status(400).json(errors);
+    return res.status(400).json(errors)
   }
   User.findOne({ email: req.body.email })
     .then(user => {
       if (user) {
-        errors.email = "Email already exists";
-        return res.status(400).json(errors);
+        errors.email = 'Email already exists'
+        return res.status(400).json(errors)
       } else {
         const avatar = gravatar.url(req.body.email, {
-          s: "200", //Size
-          r: "pg", //Rating
-          d: "mm" // Default
-        });
+          s: '200', // Size
+          r: 'pg', // Rating
+          d: 'mm' // Default
+        })
         const newUser = new User({
           name: req.body.name,
           email: req.body.email,
           avatar,
           password: req.body.password
-        });
+        })
 
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
-            if (err) throw err;
-            newUser.password = hash;
+            if (err) throw err
+            newUser.password = hash
             newUser
               .save()
               .then(user => res.json(user))
-              .catch(err => console.log(err));
-          });
-        });
+              .catch(err => console.log(err))
+          })
+        })
       }
     })
-    .catch(err => console.log(err));
-});
+    .catch(err => console.log(err))
+})
 
 // @route POST api/users/login
 // @desc Login users / Returning JWT Token
 // @access public
-router.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  User.findOne({ email }).then(user => {
-    // Check for user
-    if (!user) {
-      res.status(404).json({ email: "User Not found!" });
-    }
-    // Check Password
-    bcrypt.compare(password, user.password).then(isMatch => {
-      if (isMatch) {
-        // User match
-        const payload = {
-          id: user._id,
-          name: user.name,
-          avatar: user.avatar,
-          secret
-        }; // Create JWT Payload
-        // Sign token
-        jwt.sign(payload, secret, { expiresIn: 3600 }, (err, token) => {
-          res.json({ success: true, token: "Bearer " + token });
-        });
-      } else {
-        return res.status(400).json({ Password: "Password incorrect" });
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body)
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
+  const email = req.body.email
+  const password = req.body.password
+  User.findOne({ email })
+    .then(user => {
+      // Check for user
+      if (!user) {
+        errors.email = 'User Not found!'
+        return res.status(404).json(errors)
       }
-    });
-  });
-});
+      // Check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          // User match
+          const payload = {
+            id: user._id,
+            name: user.name,
+            avatar: user.avatar,
+            secret
+          } // Create JWT Payload
+          // Sign token
+          jwt.sign(payload, secret, { expiresIn: 3600 }, (err, token) => {
+            res.json({ success: true, token: 'Bearer ' + token })
+          })
+        } else {
+          errors.password = 'Password incorrect'
+
+          return res.status(400).json(errors)
+        }
+      })
+    })
+    .catch(err => console.log(err))
+})
 
 // @route GET api/users/current
 // @desc Return current user
 // @access Private
 router.get(
-  "/current",
-  passport.authenticate("jwt", { session: false }),
+  '/current',
+  passport.authenticate('jwt', { session: false }),
   (req, res) => {
     res.json({
       id: req.user.id,
       name: req.user.name,
       email: req.user.email
-    });
+    })
   }
-);
-module.exports = router;
+)
+module.exports = router
--- validation/register.js
+++ validation/register.js
@@ -19,21 +19,19 @@ module.exports = function validateRegisterInput (data) {
 
   if (Validator.isEmpty(data.email)) {
     errors.email = 'Email field is required'
+  } else if (!Validator.isEmail(data.email)) {
+    errors.email = 'Email is invalid'
   }
 
   if (Validator.isEmpty(data.password)) {
     errors.password = 'Password field is required'
-  }
-
-  if (Validator.isLength(data.password, { min: 6, max: 30 })) {
+  } else if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
     errors.password = 'Password must be atleast 6 characters'
   }
 
   if (Validator.isEmpty(data.password2)) {
     errors.password2 = 'Confirm Password field is required'
-  }
-
-  if (!Validator.equals(data.password, data.password2)) {
+  } else if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = 'Passwords must match'
   }
 
