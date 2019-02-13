c82ad78d5a325f4c424152864896e0985032166d
gautam-11 <gautambaruah24@gmail.com>
2019-02-13 18:57:31 +0530

    Added Like/Unlike and Comment/UnComment posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -63,7 +63,7 @@ router.post(
   }
 )
 
-// @route DELETE api/posts/
+// @route DELETE api/posts/:id
 // @desc Deletes a post
 // @access Private
 
@@ -89,6 +89,143 @@ router.delete(
         })
       })
       .catch(err => res.status(500).json(err))
+  }
+)
+
+// @route POST api/posts/like/:id
+// @desc Like post
+// @access Private
+
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id).then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyLiked: 'You have already liked this post' })
+          }
+          // Add user id to likes array
+          post.likes.unshift({ user: req.user.id })
+
+          post
+            .save()
+            .then(post => res.json(post))
+            .catch(err => res.status(500).json(err))
+        })
+      })
+      .catch(err => res.status(500).json(err))
+  }
+)
+
+// @route POST api/posts/unlike/:id
+// @desc UnLike post
+// @access Private
+
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        Post.findById(req.params.id).then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ NotLikedYet: 'You have not yet liked this post' })
+          }
+          const removeIndex = post.likes
+            .map(like => like.user.toString())
+            .indexOf(req.user.id)
+
+          post.likes.splice(removeIndex, 1)
+
+          post
+            .save()
+            .then(post => res.json(post))
+            .catch(err => res.status(500).json(err))
+        })
+      })
+      .catch(err => res.status(500).json(err))
+  }
+)
+
+// @route POST api/posts/comment/:id
+// @desc Add comment to a post
+// @access Private
+
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body)
+
+    // Check Validation
+    if (!isValid) {
+      return res.status(400).json(errors)
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        }
+        // Add to comments array
+        post.comments.unshift(newComment)
+
+        // Save
+        post
+          .save()
+          .then(post => res.json(post))
+          .catch(err => res.status(500).json(err))
+      })
+      .catch(err => res.status(404).json({ msg: 'Post not found' }))
+  }
+)
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc  Remove comment from post
+// @access Private
+
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    
+    Post.findById(req.params.id)
+      .then(post => {
+
+        // Check to see if comment exists
+        if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id )
+        .length === 0)
+        {
+          return res.status(404).json({commentNotExist : 'Comment does not exist!'})
+        }
+
+        // Get remove index
+        const removeIndex = post.comments 
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id)
+
+        post.comments.splice(removeIndex , 1)
+
+        post.save().then(post => res.json(post))
+        .catch(err => res.status(500).json({msg: 'Error in deleteing comment'}) )
+        
+      })
+      .catch(err => res.status(404).json({ msg: 'Post not found' }))
   }
 )
 
