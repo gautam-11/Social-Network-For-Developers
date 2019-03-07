7f81849e1c17bd0a88b17c30eb951a67dce1d7a7
gautam-11 <gautambaruah24@gmail.com>
2019-03-08 02:48:27 +0530

    Added state for login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor () {
+    super()
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+  onChange (e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+  onSubmit (e) {
+    e.preventDefault()
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    console.log(user)
+  }
   render () {
     return (
       <div className='login'>
@@ -11,13 +32,15 @@ class Login extends Component {
               <p className='lead text-center'>
                 Sign in to your DevConnector account
               </p>
-              <form action='dashboard.html'>
+              <form onSubmit={this.onSubmit}>
                 <div className='form-group'>
                   <input
                     type='email'
                     className='form-control form-control-lg'
                     placeholder='Email Address'
                     name='email'
+                    value={this.state.email}
+                    onChange={this.onChange}
                   />
                 </div>
                 <div className='form-group'>
@@ -26,6 +49,8 @@ class Login extends Component {
                     className='form-control form-control-lg'
                     placeholder='Password'
                     name='password'
+                    value={this.state.password}
+                    onChange={this.onChange}
                   />
                 </div>
                 <input type='submit' className='btn btn-info btn-block mt-4' />
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -11,9 +11,20 @@ class Register extends Component {
       errors: {}
     }
     this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
   }
   onChange (e) {
     this.setState({ [e.target.name]: e.target.value })
+  }
+  onSubmit (e) {
+    e.preventDefault()
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+    console.log(newUser)
   }
   render () {
     return (
@@ -25,7 +36,7 @@ class Register extends Component {
               <p className='lead text-center'>
                 Create your DevConnector account
               </p>
-              <form action='create-profile.html'>
+              <form onSubmit={this.onSubmit}>
                 <div className='form-group'>
                   <input
                     type='text'
