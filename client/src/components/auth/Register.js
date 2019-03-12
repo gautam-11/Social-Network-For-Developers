3c93a87341355bc89a65d0937e4a316ab179cb69
gautam-11 <gautambaruah24@gmail.com>
2019-03-13 02:28:02 +0530

    Added Error Handling for Sign up form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import classnames from 'classnames'
 
 class Register extends Component {
   constructor () {
@@ -24,9 +26,14 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     }
-    console.log(newUser)
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }))
   }
   render () {
+    const { errors } = this.state
+
     return (
       <div className='register'>
         <div className='container'>
@@ -36,27 +43,36 @@ class Register extends Component {
               <p className='lead text-center'>
                 Create your DevConnector account
               </p>
-              <form onSubmit={this.onSubmit}>
+              <form noValidate onSubmit={this.onSubmit}>
                 <div className='form-group'>
                   <input
                     type='text'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
                     placeholder='Name'
                     name='name'
                     value={this.state.name}
                     onChange={this.onChange}
-                    required
                   />
+                  {errors.name && (
+                    <div className='invalid-feedback'>{errors.name}</div>
+                  )}
                 </div>
                 <div className='form-group'>
                   <input
                     type='email'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
                     placeholder='Email Address'
                     name='email'
                     value={this.state.email}
                     onChange={this.onChange}
                   />
+                  {errors.email && (
+                    <div className='invalid-feedback'>{errors.email}</div>
+                  )}
                   <small className='form-text text-muted'>
                     This site uses Gravatar so if you want a profile image, use
                     a Gravatar email
@@ -65,22 +81,32 @@ class Register extends Component {
                 <div className='form-group'>
                   <input
                     type='password'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     placeholder='Password'
                     name='password'
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className='invalid-feedback'>{errors.password}</div>
+                  )}
                 </div>
                 <div className='form-group'>
                   <input
                     type='password'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     placeholder='Confirm Password'
                     name='password2'
                     value={this.state.password2}
                     onChange={this.onChange}
                   />
+                  {errors.password2 && (
+                    <div className='invalid-feedback'>{errors.password2}</div>
+                  )}
                 </div>
                 <input type='submit' className='btn btn-info btn-block mt-4' />
               </form>
