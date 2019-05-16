91aaead1b6aa758c2a058078db35ee1883fc5ecd
Gautam Baruah <gautambaruah24@gmail.com>
2019-05-16 23:50:23 +0530

    Added redux action functionality for login user method

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
+import setAuthToken from "../utils/setAuthToken";
 import axios from "axios";
+import jwt_decode from "jwt-decode";
+//Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
@@ -10,4 +13,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      //save to localStorage
+      const { token } = res.data;
+      //Set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      //Set token to Auth Header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,66 +1,93 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { loginUser } from "../../actions/authActions";
 
 class Login extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       errors: {}
-    }
-    this.onChange = this.onChange.bind(this)
-    this.onSubmit = this.onSubmit.bind(this)
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
-  onChange (e) {
-    this.setState({ [e.target.name]: e.target.value })
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
-  onSubmit (e) {
-    e.preventDefault()
+  onSubmit(e) {
+    e.preventDefault();
     const user = {
       email: this.state.email,
       password: this.state.password
-    }
-    console.log(user)
+    };
+    console.log(user);
   }
-  render () {
+  render() {
+    const { errors } = this.state;
     return (
-      <div className='login'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-8 m-auto'>
-              <h1 className='display-4 text-center'>Log In</h1>
-              <p className='lead text-center'>
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
                 Sign in to your DevConnector account
               </p>
               <form onSubmit={this.onSubmit}>
-                <div className='form-group'>
+                <div className="form-group">
                   <input
-                    type='email'
-                    className='form-control form-control-lg'
-                    placeholder='Email Address'
-                    name='email'
+                    type="email"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.email
+                    })}
+                    placeholder="Email Address"
+                    name="email"
                     value={this.state.email}
                     onChange={this.onChange}
                   />
+                  {errors.email && (
+                    <div className="invalid-feedback">{errors.email}</div>
+                  )}
                 </div>
-                <div className='form-group'>
+                <div className="form-group">
                   <input
-                    type='password'
-                    className='form-control form-control-lg'
-                    placeholder='Password'
-                    name='password'
+                    type="password"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.password
+                    })}
+                    placeholder="Password"
+                    name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  )}
                 </div>
-                <input type='submit' className='btn btn-info btn-block mt-4' />
+                <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
 
-export default Login
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+export default connect(
+  null,
+  { loginUser }
+)(Login);
