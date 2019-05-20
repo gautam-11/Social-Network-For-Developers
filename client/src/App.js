e82304327e61d1fb4c55ae3d0e5d576b7a71f691
Gautam Baruah <gautambaruah24@gmail.com>
2019-05-20 23:59:40 +0530

    Added jwt decode logic in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,46 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-import './App.css'
-import Navbar from './components/layout/Navbar'
-import Footer from './components/layout/Footer'
-import Landing from './components/layout/Landing'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
 
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
+//Check for Token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //  Decode token and get user info
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //Set User and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
-  render () {
+  render() {
     return (
       <Provider store={store}>
         <Router>
-          <div className='App'>
+          <div className="App">
             <Navbar />
-            <Route exact path='/' component={Landing} />
-            <div className='container'>
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/register' component={Register} />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
             </div>
             <Footer />
           </div>
         </Router>
       </Provider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
