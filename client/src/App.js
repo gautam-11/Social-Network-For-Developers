458da6563f9afb768ee235c0b77defd0b1d4b1ab
Gautam Baruah <gautambaruah24@gmail.com>
2019-06-05 19:33:57 +0530

    Added Create-Profile Component and Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -3,34 +3,40 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
 import PrivateRoute from "./components/common/PrivateRoute";
-import "./App.css";
+
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
-import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
-import { clearCurrentProfile } from "./actions/profileActions";
+import CreateProfile from "./components/create-profile/CreateProfile";
 
-//Check for Token
+import "./App.css";
+
+// Check for token
 if (localStorage.jwtToken) {
   // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
-  //  Decode token and get user info
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
-  //Set User and isAuthenticated
+  // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
-  //Check for expired token
+
+  // Check for expired token
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
+    // Logout user
     store.dispatch(logoutUser());
-    //Clear current profile
+    // Clear current Profile
     store.dispatch(clearCurrentProfile());
-    //Redirect to Login
+    // Redirect to login
     window.location.href = "/login";
   }
 }
@@ -44,10 +50,17 @@ class App extends Component {
             <Navbar />
             <Route exact path="/" component={Landing} />
             <div className="container">
-              <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,18 +1,23 @@
 import axios from "axios";
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from "./types";
+import {
+  GET_PROFILE,
+  PROFILE_LOADING,
+  CLEAR_CURRENT_PROFILE,
+  GET_ERRORS
+} from "./types";
 
 // Get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
   axios
     .get("/api/profile")
-    .then(res => {
+    .then(res =>
       dispatch({
         type: GET_PROFILE,
         payload: res.data
-      });
-    })
+      })
+    )
     .catch(err =>
       dispatch({
         type: GET_PROFILE,
@@ -21,13 +26,27 @@ export const getCurrentProfile = () => dispatch => {
     );
 };
 
-//Profile Loading
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+  axios
+    .post("/api/profile", profileData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Profile loading
 export const setProfileLoading = () => {
   return {
     type: PROFILE_LOADING
   };
 };
-//Clear Profile
+
+// Clear profile
 export const clearCurrentProfile = () => {
   return {
     type: CLEAR_CURRENT_PROFILE
