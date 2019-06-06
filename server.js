588918c30ab7d9c8f0eda649bf3e9e7affa04d1c
Gautam Baruah <gautambaruah24@gmail.com>
2019-06-06 20:28:42 +0530

    prepared for deployment

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
-const bodyParser = require('body-parser')
-const passport = require('passport')
+const express = require("express");
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // DB config
-const db = require('./config/keys').mongoURI
+const db = require("./config/keys").mongoURI;
 
 // Connect to mongodb
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongodb')
+    console.log("Connected to Mongodb");
   })
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 // Passport middleware
-app.use(passport.initialize())
+app.use(passport.initialize());
 
 // Passport Config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
 // Use Routes
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-const port = process.env.PORT || 4000
+// Serve static assets if in production
+if (process.env.NODE_ENV == "production") {
+  // Set Static server
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
